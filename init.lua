97b291932ad695b128c30245d95470a2d1af2b34
Kylejeong2 <Kylejeong21@gmail.com>
2025-03-29 00:21:16 -0700

    fix tree

--- init.lua
+++ init.lua
@@ -92,13 +92,6 @@ P.S. You can delete this when you're done too. It's your config now! :)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
--- disable netrw at the very start of your init.lua
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-
--- optionally enable 24-bit colour
-vim.opt.termguicolors = true
-
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = false
 
