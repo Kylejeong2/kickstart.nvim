7cb10dd36ef4856754ccbf4a2cf91ea4eb16f142
Kylejeong2 <kylejeong21@gmail.com>
2025-07-29 12:49:47 -0700

    visible dotfiles, autoclose

--- lua/custom/plugins/neo-tree.lua
+++ lua/custom/plugins/neo-tree.lua
@@ -8,5 +8,15 @@ return {
       'nvim-tree/nvim-web-devicons', -- optional, but recommended
     },
     lazy = false, -- neo-tree will lazily load itself
+    opts = {
+      close_if_last_window = true, -- Close Neo-tree if it is the last window left in the tab
+      filesystem = {
+        filtered_items = {
+          visible = true, -- Show hidden files
+          hide_dotfiles = false, -- Don't hide dotfiles
+          hide_gitignored = false, -- Don't hide gitignored files
+        },
+      },
+    },
   },
 }
